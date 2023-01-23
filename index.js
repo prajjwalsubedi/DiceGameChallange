1b8039d3ee6d39631504bbefcd0fc69017a853e4
Prajjwal Subedi <66186489+prajjwalsubedi@users.noreply.github.com>
2023-01-23 21:39:46 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var firstRandom = Math.floor(Math.random() * 6 + 1)
 var secondRandom = Math.floor(Math.random() * 6 + 1)
 
-var image1 = "images/dice"+firstRandom+".png";
-var image2 = "images/dice"+secondRandom+".png";
+var image1 = "./images/dice"+firstRandom+".png";
+var image2 = "./images/dice"+secondRandom+".png";
 
 console.log(document.querySelectorAll("img")[0].setAttribute("src", image1));
 console.log(document.querySelectorAll("img")[1].setAttribute("src", image2));
